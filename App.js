6179642c072972e20f96102d5964a7c1ce5c64cb
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-05-13 18:32:41 -0300

    inicio do projeto de consumo de api via cep

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import Cep from './components/Cep';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.bloco}>
+          <Text style={styles.texto}>Digite seu CEP</Text>
+
+          <TextInput 
+            placeholder='ex: 11740000'
+            keyboardType='numeric'
+            style={styles.input}
+          />
+
+          <TouchableOpacity 
+            style={styles.botao}
+          >
+              <Text style={styles.txtBotao}>Buscar</Text>
+          </TouchableOpacity>
+
+          <Cep />
+      </View>
     </View>
   );
 }
@@ -17,4 +33,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  bloco:{
+    width:'100%',
+    alignItems: 'center',
+  },
+  texto:{
+    fontSize:20,
+  },
+  input:{
+    width:'80%',
+    marginLeft:'5%',
+    borderBottomWidth:2,
+    marginTop:20,
+    fontSize:30
+  },
+  botao:{
+    width:'80%',
+    marginTop:30
+  },
+  txtBotao:{
+    fontSize:20,
+    textAlign:'center'
+  }
+
 });
